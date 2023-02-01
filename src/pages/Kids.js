e83d685183eda0f376c0e8ac83b1581a55b58324
DG_LEE <dongq0513@gmail.com>
2023-02-01 11:41:08 +0900

    junior 추가

--- src/pages/Kids.js
+++ src/pages/Kids.js
@@ -22,6 +22,42 @@ export default function Kids() {
                 return (
                   <>
                     {data.type01 === 'kids' ? 
+                      <div className="pro">
+                        <Link to={`/Kids/detail/${i}`}>
+                          <img src={data.image} alt='' style={{width: '100%'}}/>
+                          <div className="txt_box">
+                            <div className="condition">{data.condition}</div>
+                            <div className="title">{data.title}</div>
+                            <div className="desc">{data.desc}</div>
+                            <div className="color">{data.color}</div>
+                            <div className="price_box">
+                              <div className="price">{((data.price) * (1-data.discount)).toLocaleString()} 원</div>
+                              {data.discount === 0 ? "" : <div className="origin">{data.price.toLocaleString()} 원</div>}
+                            </div>  
+                            {data.discount === 0 ? "" : <div className="discount">{(data.discount) * 100 + '% 할인'}</div>}
+                          </div>
+                        </Link> 
+                      </div> 
+                    : 
+                      data.type01 === 'baby' ?
+                      <div className="pro">
+                        <Link to={`/Kids/detail/${i}`}>
+                          <img src={data.image} alt='' style={{width: '100%'}}/>
+                          <div className="txt_box">
+                            <div className="condition">{data.condition}</div>
+                            <div className="title">{data.title}</div>
+                            <div className="desc">{data.desc}</div>
+                            <div className="color">{data.color}</div>
+                            <div className="price_box">
+                              <div className="price">{((data.price) * (1-data.discount)).toLocaleString()} 원</div>
+                              {data.discount === 0 ? "" : <div className="origin">{data.price.toLocaleString()} 원</div>}
+                            </div>  
+                            {data.discount === 0 ? "" : <div className="discount">{(data.discount) * 100 + '% 할인'}</div>}
+                          </div>
+                        </Link> 
+                      </div>
+                    :
+                    data.type01 === 'junior' ?
                     <div className="pro">
                       <Link to={`/Kids/detail/${i}`}>
                         <img src={data.image} alt='' style={{width: '100%'}}/>
@@ -37,25 +73,8 @@ export default function Kids() {
                           {data.discount === 0 ? "" : <div className="discount">{(data.discount) * 100 + '% 할인'}</div>}
                         </div>
                       </Link> 
-                    </div> : 
-                    data.type01 === 'baby' ?
-                    <div className="pro">
-                      <Link to={`/Kids/detail/${i}`}>
-                        <img src={data.image} alt='' style={{width: '100%'}}/>
-                        <div className="txt_box">
-                          <div className="condition">{data.condition}</div>
-                          <div className="title">{data.title}</div>
-                          <div className="desc">{data.desc}</div>
-                          <div className="color">{data.color}</div>
-                          <div className="price_box">
-                            <div className="price">{((data.price) * (1-data.discount)).toLocaleString()}</div>
-                            {data.discount === 0 ? "" : <div className="origin">{data.price.toLocaleString()}</div>}
-                          </div>  
-                          {data.discount === 0 ? "" : <div className="discount">{(data.discount) * 100 + '% 할인'}</div>}
-                        </div>
-                      </Link> 
                     </div>
-                    : ""}
+                  :""}
                   </>     
                 )
               })
